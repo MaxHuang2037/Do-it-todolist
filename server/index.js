ab62ef99f9a620c68b8a82d65cc6a8c11d51cadd
Max Huang <92611907+MaxHuang2037@users.noreply.github.com>
2024-12-29 12:36:40 -0500

    small changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 // database
 const connectDB = require("./db/connect")
@@ -8,9 +9,11 @@ const connectDB = require("./db/connect")
 const userRouter = require("./routes/todoItem")
 app.use("/", bodyParser.json(), userRouter)
 
+const URI = process.env.MONGO_URI
+
 const start = async() => {
     try{
-        await connectDB("mongodb+srv://vmkcreeper:gsbzss777@nodetaskmanager.7y7vy.mongodb.net/?retryWrites=true&w=majority")
+        await connectDB(URI)
         app.listen(5000, console.log("Server running on port 5000"))
     } catch (error){
         console.log(error)
